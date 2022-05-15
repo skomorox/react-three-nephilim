0418f9fbca70fd56c624521251cdc2fbef700f90
Alex <o.kryvosheiev@venstar.com>
2022-05-15 19:33:16 +0300

    Line class

--- src/Decoration/Line.js
+++ src/Decoration/Line.js
@@ -1,35 +1,37 @@
 /*****************************************************************************************************
  * @author Skomorox
  * @class Line
- * Abstract: Class Line. THREE.Line wrapper.
+ * Abstract: Class Line. THREE.Line, THREE.LineSegments wrapper.
  *****************************************************************************************************
  */
 
-import * as Three from 'three';
-import { Decoration } from './Decoration';
-
-export class Line extends Decoration {
-
-  constructor({ vertices, material }) {
-    super();
-    this.setMaterial(material);
-    this.setGeomerty(vertices); 
-  }
-
-  setMaterial = ({ type, params }) => {
-    this.material = new Three[`Line${this.manager.capitalize(type)}Material`](params);
-  };
-
-  setGeomerty = vertices => {
-    this.geometry = new Three.Geometry();
-    this.geometry.vertices = vertices.map(({ x, y, z }) => new Three.Vector3(x, y, z));
-    this.geometry.computeLineDistances();
-    this.visual = new Three.Line(this.geometry, this.material);
-  };
-
-  updateMaterial = material => {
-    this.setMaterial(material);
-    this.visual.material = this.material;
-  };
-  
-}
+ import * as Three from 'three';
+ import { Decoration } from './Decoration';
+ 
+ export class Line extends Decoration {
+ 
+   constructor({ type, geometry, material }) {
+     super();
+     this.setMaterial(material);
+     this.setGeomerty(type, geometry);
+   }
+ 
+   setMaterial = ({ type, params }) => {
+     this.material = new Three[`Line${this.manager.capitalize(type)}Material`](params);
+   };
+ 
+   setGeomerty = (type, geometry) => {
+     this.geometry = geometry;
+     this.visual = new Three[type === 'line' ?
+      'Line' :
+      `Line${this.manager.capitalize(type || '')}`
+    ](this.geometry, this.material);
+   };
+ 
+   updateMaterial = material => {
+     this.setMaterial(material);
+     this.visual.material = this.material;
+   };
+   
+ }
+ 
